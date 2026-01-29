70e5f3c6066bccbb089197098c69f5f3fcf38a21
UnaxGainzarain <unaxarbizu1@gmail.com>
2026-01-29 19:21:37 +0100

    feat: implementado propiedades de archivador

--- src/main/java/ArchivadorService.java
+++ src/main/java/ArchivadorService.java
@@ -2,7 +2,24 @@
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
-
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import java.io.File;
+import java.io.FileWriter;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 public class ArchivadorService {
-	
+	public static void main(String[] args) {
+	Properties props = new Properties();
+    props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    // IMPORTANTE: Group ID único para archivado [cite: 190]
+    props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "grupo-archivado");
+    props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+    KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+    consumer.subscribe(Collections.singletonList("trabajos-entrada"));
+	}
 }
